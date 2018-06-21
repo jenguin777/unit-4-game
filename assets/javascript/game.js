2912ab8e185af19e031541ce2cbc12997ec8a5be
Jennifer Fahey <jenguin777@gmail.com>
2018-06-21 10:43:58 -0400

    checking in styling changes and some jQuery - jQuery not working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,31 +2,39 @@ $(document).ready(function () {
 
     //Define global variables
 
-
-    //Create functions
-
-
-
-
-    //Define business logic
-
     var targetNumber;
 
     $("#number-to-guess").text(targetNumber);
 
     var counter = 0;
+    var losses = 0;
+    var wins = 0;
+    // var pictures {
+    //     image1 : "../images/BlueCrystal200x200.jpg",
+    //     image2 : "../images/FuschiaCrystal596x745.jpg",
+    //     image3 : "../images/GoldCrystal650x650.jpg",
+    //     image4 : "../images/GrayBlueCrystal420x420.jpg"
+    // };
+
+    // $("#wins").append();
+    // console.log("wins: " + wins);
 
     // Now we want to create multiple crystals each with their own unique number value between 19 and 120 (inclusive).
     function startGame () {
         targetNumber = Math.floor(Math.random() * (120 -50 + 1)) + 50;
         console.log(targetNumber);
+        $("#wins").append();
+        console.log("wins: " + wins);
+        $("#losses").append();
+        $("#target-number").append();
         renderCrystals();
     }
 
-    // Next we create a for loop to create 4 crystals 
+    //The purpose of this function is to render crystals on the screen
     function renderCrystals() {
-        //Before we get started, we need to empty out the old crystals
+        //Before we get started, we need to empty out any existing crystals from previous games
         $("#crystals").empty();
+        //Now we want to make sure we loop 4 times
         for (var i = 0; i < 4; i++) {
             // We want to assign a random number for each crystal between 1 and 12 (inclusive)
             var randomNumber = Math.floor(Math.random() * (12 - 1 + 1)) + 1;
@@ -38,8 +46,11 @@ $(document).ready(function () {
             // This will allow the CSS to take effect.
             imageCrystal.addClass("crystal-image");
     
-            // Each imageCrystal will be given a src link to the crystal image
-            imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+            // Each imageCrystal will be given a src link to the crystal image - need to enhance this to show 4 crystals and apply same logic to each
+            // imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
+
+            imageCrystal.attr("src", "../assets/images/GrayBlueCrystal420x420.jpg");
+
     
             // Each imageCrystal will be given a data attribute called data-crystalValue.
             // This data attribute will be set equal to the array value.
@@ -50,6 +61,8 @@ $(document).ready(function () {
         }    
     }
    
+    startGame();
+
     // This time, our click event applies to every single crystal on the page. Not just one.
     $(document).on("click", ".crystal-image", function () {
 
@@ -69,14 +82,18 @@ $(document).ready(function () {
 
         if (counter === targetNumber) {
             alert("You win!");
+            wins++;
+            $("#wins").append();
             startGame();
         }
 
         else if (counter > targetNumber) {
             alert("You lose!!");
+            losses++;
+            $("#losses").append();
             startGame();
         }
 
     });
     startGame();
-})
+});
